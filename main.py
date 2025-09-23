23b5a2d309fb3ba358e42698252e8f1a8258d187
Raj Singh <rajbgi377@gmail.com>
2025-09-23 01:29:39 -0700

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,10 @@ col1, col2, col3 = st.columns(3)
 with col2:
     st.title("eSwasthya")
     st.write("Your Health, Our Priority")
+    st.video("drvideo1.mp4", format="video/mp4", start_time=0)
+    st.video("drvideo2.mp4", format="video/mp4", start_time=0)
+    st.video("drvideo3.mp4", format="video/mp4", start_time=0)
+
 
 
 
