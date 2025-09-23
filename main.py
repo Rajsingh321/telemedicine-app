1bb08abb748698979596f856425f5b7c154bda21
Raj Singh <rajbgi377@gmail.com>
2025-09-23 00:57:06 -0700

    Update main.py

--- main.py
+++ main.py
@@ -5,8 +5,9 @@ st.set_page_config(page_title="eSwasthya", page_icon=":hospital:", layout="wide"
 col1, col2, col3 = st.columns(3)
 
 with col2:
-    st.title("Welcome to eSwasthya")
-    st.header("Your Health, Our Priority")
+    st.markdown("Welcome to eSwasthya")
+    st.write("Your Health, Our Priority")
 
 st.write("it's working")
+
 
